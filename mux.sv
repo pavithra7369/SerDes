e00c6b56fe3f3738ee5edc74f3d8fe1b5231a10c
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 08:22:57 +0530

    Create mux.sv

---
+++ mux.sv
@@ -0,0 +1,13 @@
+module day1 (
+  input   wire [7:0]    a_i,
+  input   wire [7:0]    b_i,
+  input   wire          sel_i,
+  output  wire [7:0]    y_o
+);
+  // Write your logic here...
+  assign y_o=(sel_i)?b_i:_i;
+
+endmodule
+
+
+
